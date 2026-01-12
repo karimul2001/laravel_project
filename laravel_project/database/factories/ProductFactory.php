a82b66949ea5a09d7502b0c0125445f0bdff4d1b
karimul2001 <karimul.bd501@gmail.com>
2026-01-12 13:36:30 +0600

    Create Seeder

---
+++ laravel_project/database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'details' => fake()->sentence(),
+            'sku' => Str::random(10),
+            'stock' =>rand(1, 20),
+            'price' =>rand(20.00, 5000.00),
+            'image' => 'product_photo/nophoto.jpg',
+            'category_id' => rand(1, 4)
+        ];
+    }
+}
+// id name details sku stock price image category_id
